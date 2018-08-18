e2866836a4842ca76ae8a19819507dace1474492
Emmanuel Jomer Baling <ej.baling@gmail.com>
2018-08-18 13:31:21 +1200

    SignalR notification initial implementation.

---
+++ Pages/Notification.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homeautomation.Pages
+{
+    public class NotificationModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
